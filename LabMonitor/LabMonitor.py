efad7c84e48695a5394c30cefc869b0eae9039e9
Will Yager <will.yager@gmail.com>
2016-04-04 17:15:58 -0500

    Python 3

--- LabMonitor/LabMonitor.py
+++ LabMonitor/LabMonitor.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     bpg = BPG400.BPG400("/dev/ttyUSB0")
     while True:
         time.sleep(0.5) # Just so we don't spam the console.
-        print({"{0:.3e} mbar".format(bpg.read().mbar())})
+        print("{0:.3e} mbar".format(bpg.read().mbar()))
