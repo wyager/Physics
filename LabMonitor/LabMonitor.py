2a729f4b9203bb75cff49d7a212f845f42ce5b87
Will Yager <will.yager@gmail.com>
2016-04-04 16:58:05 -0500

    Python 3

--- LabMonitor/LabMonitor.py
+++ LabMonitor/LabMonitor.py
@@ -3,7 +3,7 @@ import BPG400
 import time
 
 if __name__ == '__main__':
-    bpg = BPG400("/dev/ttyUSB0")
+    bpg = BPG400.BPG400("/dev/ttyUSB0")
     while True:
         time.sleep(0.5) # Just so we don't spam the console.
         print({"{0:.3e} mbar".format(bpg.read().mbar())})
